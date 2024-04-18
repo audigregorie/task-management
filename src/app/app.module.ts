5b95853418a00a16b2079cbbed1282126567454d
Audi Young <audi.mycloud@gmail.com>
2024-04-18 04:33:44 +0100

    Style: Add only UI for task list component
    - The component does not currently have any functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { TableModule } from 'primeng/table'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HomeComponent } from './modules/pages/home/home.component';
-import { ManageTasksComponent } from './modules/tasks/manage-tasks/manage-tasks.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './shared/components/header/header.component'
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component'
+import { FooterComponent } from './shared/components/footer/footer.component'
+import { HomeComponent } from './modules/pages/home/home.component'
+import { ManageTasksComponent } from './modules/tasks/manage-tasks/manage-tasks.component'
+import { TaskListComponent } from './modules/tasks/task-list/task-list.component'
 
 @NgModule({
   declarations: [
@@ -16,13 +18,11 @@ import { ManageTasksComponent } from './modules/tasks/manage-tasks/manage-tasks.
     SidebarComponent,
     FooterComponent,
     HomeComponent,
-    ManageTasksComponent
+    ManageTasksComponent,
+    TaskListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, TableModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
